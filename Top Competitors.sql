fdce235562ea3ddb927f783f4c9e4c286f30adae
Iti Tyagi <intechworx@gmail.com>
2018-09-19 14:02:05 +0200

    Update Top Competitors.sql

--- Top Competitors.sql
+++ Top Competitors.sql
@@ -7,4 +7,18 @@ If more than one hacker received full scores in same number of challenges, then 
 
 More details can be found at: https://www.hackerrank.com/challenges/full-score/problem
 
-Solution (coming by end of today)
+Solution 
+
+select h.hacker_id, h.name
+from submissions s
+inner join challenges c
+on s.challenge_id = c.challenge_id
+inner join difficulty d
+on c.difficulty_level = d.difficulty_level 
+inner join hackers h
+on s.hacker_id = h.hacker_id
+where s.score = d.score and c.difficulty_level = d.difficulty_level
+group by h.hacker_id, h.name
+having count(s.hacker_id) > 1
+order by count(s.hacker_id) desc, s.hacker_id asc
+
